4ad6c86fc7527456b51b57dc17a75eb380f2836b
ershov_vladislav@softcom.su <ershov_vladislav@softcom.su>
2023-03-13 09:36:49 +0300

    Reformat code

--- ErshovVladislav/Task1/src/Main.java
+++ ErshovVladislav/Task1/src/Main.java
@@ -1,12 +1,15 @@
 import mpi.MPI;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
-    private final static String FILE_NAME_WITH_INITIAL_ARRAY = "../resources/initialArray.txt";
+    private final static String FILE_NAME_WITH_INITIAL_ARRAY = "../resources/initialArray.txt"; //$NON-NLS-1$
 
     public static void main(String[] args) {
         // fillInitialArray(FILE_NAME_WITH_INITIAL_ARRAY);
@@ -54,7 +57,8 @@ public class Main {
 
             for (int i = 1; i < size; i++) {
                 int[] receiveBufferBoundaryNumbers = new int[receiveBufferBoundaryNumbersSize[i]];
-                MPI.COMM_WORLD.Recv(receiveBufferBoundaryNumbers, 0, receiveBufferBoundaryNumbersSize[i], MPI.INT, i, 12);
+                MPI.COMM_WORLD.Recv(receiveBufferBoundaryNumbers, 0, receiveBufferBoundaryNumbersSize[i], MPI.INT, i,
+                        12);
 
                 for (int number : receiveBufferBoundaryNumbers) {
                     allBoundaryNumbers.add(number);
@@ -97,7 +101,7 @@ public class Main {
             int[] receiveBufferLeadingNumbers = new int[receiveBufferLeadingNumbersSize[0]];
             MPI.COMM_WORLD.Recv(receiveBufferLeadingNumbers, 0, receiveBufferLeadingNumbersSize[0], MPI.INT, 0, 22);
 
-            for (int number: receiveBufferLeadingNumbers) {
+            for (int number : receiveBufferLeadingNumbers) {
                 leadingNumbers.add(number);
             }
         }
@@ -105,7 +109,7 @@ public class Main {
         List<List<Integer>> sentSequencesNumbers = getSentSequencesNumbers(nodeArray, leadingNumbers, size);
 
         List<Integer> receiveSequenceNumbersSizes = new ArrayList<>();
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             if (rank < i) {
                 int[] sendBufferSequenceNumbersSize = { sentSequencesNumbers.get(i).size() };
                 MPI.COMM_WORLD.Send(sendBufferSequenceNumbersSize, 0, 1, MPI.INT, i, 31);
@@ -130,7 +134,7 @@ public class Main {
             receiveSequencesNumbers.add(new ArrayList<>());
         }
 
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             if (rank < i) {
                 int[] sendBufferSequenceNumbers = new int[sentSequencesNumbers.get(i).size()];
                 for (int j = 0; j < sentSequencesNumbers.get(i).size(); j++) {
@@ -139,16 +143,18 @@ public class Main {
                 MPI.COMM_WORLD.Send(sendBufferSequenceNumbers, 0, sentSequencesNumbers.get(i).size(), MPI.INT, i, 32);
 
                 int[] receiveBufferSequenceNumbers = new int[receiveSequenceNumbersSizes.get(i)];
-                MPI.COMM_WORLD.Recv(receiveBufferSequenceNumbers, 0, receiveSequenceNumbersSizes.get(i), MPI.INT, i, 32);
+                MPI.COMM_WORLD.Recv(receiveBufferSequenceNumbers, 0, receiveSequenceNumbersSizes.get(i), MPI.INT, i,
+                        32);
 
-                for (int number: receiveBufferSequenceNumbers) {
+                for (int number : receiveBufferSequenceNumbers) {
                     receiveSequencesNumbers.get(i).add(number);
                 }
             } else if (rank > i) {
                 int[] receiveBufferSequenceNumbers = new int[receiveSequenceNumbersSizes.get(i)];
-                MPI.COMM_WORLD.Recv(receiveBufferSequenceNumbers, 0, receiveSequenceNumbersSizes.get(i), MPI.INT, i, 32);
+                MPI.COMM_WORLD.Recv(receiveBufferSequenceNumbers, 0, receiveSequenceNumbersSizes.get(i), MPI.INT, i,
+                        32);
 
-                for (int number: receiveBufferSequenceNumbers) {
+                for (int number : receiveBufferSequenceNumbers) {
                     receiveSequencesNumbers.get(i).add(number);
                 }
 
@@ -163,7 +169,7 @@ public class Main {
         }
 
         List<Integer> finalNodeNumbers = new ArrayList<>();
-        for (List<Integer> receiveSequenceNumbers: receiveSequencesNumbers) {
+        for (List<Integer> receiveSequenceNumbers : receiveSequencesNumbers) {
             finalNodeNumbers.addAll(receiveSequenceNumbers);
         }
 
@@ -171,11 +177,11 @@ public class Main {
         Collections.sort(finalNodeNumbers);
 
         // Print sorted array
-        printNodeArray("Answer:", finalNodeNumbers, rank, size);
+        printNodeArray("Answer:", finalNodeNumbers, rank, size); //$NON-NLS-1$
 
         if (rank == 0) {
-            System.out.println("Size: " + size);
-            System.out.println("Time:");
+            System.out.println("Size: " + size); //$NON-NLS-1$
+            System.out.println("Time:"); //$NON-NLS-1$
             System.out.println(((double) (System.currentTimeMillis() - time)) / 100);
         }
 
@@ -186,25 +192,26 @@ public class Main {
      * Fill initial array in file.
      *
      * @param fileName file name
-     *
-     * @throws IllegalArgumentException if fileName is null, blank or empty or the file with the given fileName cannot be written to
+     * @throws IllegalArgumentException if fileName is null, blank or empty or the
+     *                                  file with the given fileName cannot be
+     *                                  written to
      */
     public static void fillInitialArray(String fileName) throws IllegalArgumentException {
         if (fileName == null) {
-            throw new IllegalArgumentException("The given fileName must not be null");
+            throw new IllegalArgumentException("The given fileName must not be null"); //$NON-NLS-1$
         } else if (fileName.isBlank() || fileName.isEmpty()) {
-            throw new IllegalArgumentException("The given fileName must not be blank or empty");
+            throw new IllegalArgumentException("The given fileName must not be blank or empty"); //$NON-NLS-1$
         }
 
-        try(FileWriter writer = new FileWriter(fileName, false)) {
+        try (FileWriter writer = new FileWriter(fileName, false)) {
             int randomNum;
             for (int i = 0; i < 10000000; i++) {
                 randomNum = ThreadLocalRandom.current().nextInt(0, 1000000 + 1);
-                writer.write(randomNum + " ");
+                writer.write(randomNum + " "); //$NON-NLS-1$
             }
             writer.flush();
-        } catch(IOException ex) {
-            throw new IllegalArgumentException("The file with the given fileName cannot be written to");
+        } catch (IOException ex) {
+            throw new IllegalArgumentException("The file with the given fileName cannot be written to"); //$NON-NLS-1$
         }
     }
 
@@ -213,18 +220,17 @@ public class Main {
      *
      * @param nodeArray target array
      * @param size      number of nodes
-     *
      * @return list of boundary numbers
-     *
-     * @throws IllegalArgumentException if nodeArray is null, nodeArray contain null values or size < 1
+     * @throws IllegalArgumentException if nodeArray is null, nodeArray contain null
+     *                                  values or size < 1
      */
     public static List<Integer> getBoundaryNumbers(List<Integer> nodeArray, int size) throws IllegalArgumentException {
         if (nodeArray == null) {
-            throw new IllegalArgumentException("The given nodeArray must not be null");
+            throw new IllegalArgumentException("The given nodeArray must not be null"); //$NON-NLS-1$
         } else if (size < 1) {
-            throw new IllegalArgumentException("The given size must be greater than one");
+            throw new IllegalArgumentException("The given size must be greater than one"); //$NON-NLS-1$
         } else if (nodeArray.contains(null)) {
-            throw new IllegalArgumentException("In the given nodeArray must not contain null values");
+            throw new IllegalArgumentException("In the given nodeArray must not contain null values"); //$NON-NLS-1$
         } else if (nodeArray.isEmpty()) {
             return new ArrayList<>();
         } else if (nodeArray.size() < size) {
@@ -247,22 +253,23 @@ public class Main {
      * @param allBoundaryNumbers all boundary numbers from all nodes
      * @param rank               node number
      * @param size               number of nodes
-     *
      * @return list of leading numbers
-     *
-     * @throws IllegalArgumentException if allBoundaryNumbers is null, allBoundaryNumbers contain null values, size < 1, rank < 0 or rank >= size
+     * @throws IllegalArgumentException if allBoundaryNumbers is null,
+     *                                  allBoundaryNumbers contain null values, size
+     *                                  < 1, rank < 0 or rank >= size
      */
-    public static List<Integer> computeLeadingNumbers(List<Integer> allBoundaryNumbers, int rank, int size) throws IllegalArgumentException {
+    public static List<Integer> computeLeadingNumbers(List<Integer> allBoundaryNumbers, int rank, int size)
+            throws IllegalArgumentException {
         if (allBoundaryNumbers == null) {
-            throw new IllegalArgumentException("The given array must not be null");
+            throw new IllegalArgumentException("The given array must not be null"); //$NON-NLS-1$
         } else if (allBoundaryNumbers.contains(null)) {
-            throw new IllegalArgumentException("In the given allBoundaryNumbers must not contain null values");
+            throw new IllegalArgumentException("In the given allBoundaryNumbers must not contain null values"); //$NON-NLS-1$
         } else if (rank < 0) {
-            throw new IllegalArgumentException("The given rank must be greater than zero");
+            throw new IllegalArgumentException("The given rank must be greater than zero"); //$NON-NLS-1$
         } else if (size < 1) {
-            throw new IllegalArgumentException("The given size must be greater than one");
+            throw new IllegalArgumentException("The given size must be greater than one"); //$NON-NLS-1$
         } else if (rank >= size) {
-            throw new IllegalArgumentException("The given rank must be less than given size");
+            throw new IllegalArgumentException("The given rank must be less than given size"); //$NON-NLS-1$
         } else if (rank != 0) {
             return new ArrayList<>();
         }
@@ -270,7 +277,8 @@ public class Main {
         List<Integer> leadingNumbers = new ArrayList<>();
         for (int i = 0; i < size - 1; i++) {
             int index = (i + 1) * size + size / 2 - 1;
-            if (index > allBoundaryNumbers.size() - 1 || index < 0) break;
+            if (index > allBoundaryNumbers.size() - 1 || index < 0)
+                break;
             leadingNumbers.add(allBoundaryNumbers.get(index));
         }
         return leadingNumbers;
@@ -281,16 +289,15 @@ public class Main {
      *
      * @param initialArray initial array
      * @param size         number of nodes
-     *
      * @return list of left and right borders
-     *
      * @throws IllegalArgumentException if initialArray is null or size < 1
      */
-    public static List<List<Integer>> getNodeBorders(List<Integer> initialArray, int size) throws IllegalArgumentException {
+    public static List<List<Integer>> getNodeBorders(List<Integer> initialArray, int size)
+            throws IllegalArgumentException {
         if (initialArray == null) {
-            throw new IllegalArgumentException("The given initialArray must not be null");
+            throw new IllegalArgumentException("The given initialArray must not be null"); //$NON-NLS-1$
         } else if (size < 1) {
-            throw new IllegalArgumentException("The given size must be greater than one");
+            throw new IllegalArgumentException("The given size must be greater than one"); //$NON-NLS-1$
         }
 
         List<Integer> leftBorders = new ArrayList<>();
@@ -317,16 +324,15 @@ public class Main {
      * Get initial array from file.
      *
      * @param fileName file name
-     *
      * @return initial array
-     *
-     * @throws IllegalArgumentException if fileName is null, blank or empty or file with the given fileName not found
+     * @throws IllegalArgumentException if fileName is null, blank or empty or file
+     *                                  with the given fileName not found
      */
     public static List<Integer> getInitialArrayFromFile(String fileName) throws IllegalArgumentException {
         if (fileName == null) {
-            throw new IllegalArgumentException("The given fileName must not be null");
-        }  else if (fileName.isBlank() || fileName.isEmpty()) {
-            throw new IllegalArgumentException("The given fileName must not be blank or empty");
+            throw new IllegalArgumentException("The given fileName must not be null"); //$NON-NLS-1$
+        } else if (fileName.isBlank() || fileName.isEmpty()) {
+            throw new IllegalArgumentException("The given fileName must not be blank or empty"); //$NON-NLS-1$
         }
 
         List<Integer> initialArray = new ArrayList<>();
@@ -337,7 +343,7 @@ public class Main {
                 initialArray.add(scanner.nextInt());
             }
         } catch (FileNotFoundException ex) {
-            throw new IllegalArgumentException("File with the given fileName not found");
+            throw new IllegalArgumentException("File with the given fileName not found"); //$NON-NLS-1$
         }
 
         return initialArray;
@@ -349,18 +355,18 @@ public class Main {
      * @param nodeArray      array on node
      * @param leadingNumbers leading numbers
      * @param size           number of nodes
-     *
      * @return list of sequences of numbers
-     *
-     * @throws IllegalArgumentException if nodeArray or leadingNumbers is null, size < 1
+     * @throws IllegalArgumentException if nodeArray or leadingNumbers is null, size
+     *                                  < 1
      */
-    public static List<List<Integer>> getSentSequencesNumbers(List<Integer> nodeArray, List<Integer> leadingNumbers, int size) throws IllegalArgumentException {
+    public static List<List<Integer>> getSentSequencesNumbers(List<Integer> nodeArray, List<Integer> leadingNumbers,
+                                                              int size) throws IllegalArgumentException {
         if (nodeArray == null) {
-            throw new IllegalArgumentException("The given nodeArray must not be null");
+            throw new IllegalArgumentException("The given nodeArray must not be null"); //$NON-NLS-1$
         } else if (leadingNumbers == null) {
-            throw new IllegalArgumentException("The given leadingNumbers must not be null");
+            throw new IllegalArgumentException("The given leadingNumbers must not be null"); //$NON-NLS-1$
         } else if (size < 1) {
-            throw new IllegalArgumentException("The given size must be greater than one");
+            throw new IllegalArgumentException("The given size must be greater than one"); //$NON-NLS-1$
         }
 
         List<List<Integer>> sentSequencesNumbers = new ArrayList<>();
@@ -369,8 +375,8 @@ public class Main {
         }
 
         int j = 0;
-        for (Integer number: nodeArray) {
-            while(j < leadingNumbers.size() && number > leadingNumbers.get(j)) {
+        for (Integer number : nodeArray) {
+            while (j < leadingNumbers.size() && number > leadingNumbers.get(j)) {
                 j++;
             }
             sentSequencesNumbers.get(j).add(number);
@@ -386,20 +392,21 @@ public class Main {
      * @param array array on node
      * @param rank  node number
      * @param size  number of nodes
-     *
-     * @throws IllegalArgumentException if title is null, array is null, size < 1, rank < 0 or rank >= size
+     * @throws IllegalArgumentException if title is null, array is null, size < 1,
+     *                                  rank < 0 or rank >= size
      */
-    private static void printNodeArray(String title, List<Integer> array, int rank, int size) throws IllegalArgumentException {
+    private static void printNodeArray(String title, List<Integer> array, int rank, int size)
+            throws IllegalArgumentException {
         if (title == null) {
-            throw new IllegalArgumentException("The given title must not be null");
+            throw new IllegalArgumentException("The given title must not be null"); //$NON-NLS-1$
         } else if (array == null) {
-            throw new IllegalArgumentException("The given array must not be null");
+            throw new IllegalArgumentException("The given array must not be null"); //$NON-NLS-1$
         } else if (rank < 0) {
-            throw new IllegalArgumentException("The given rank must be greater than zero");
+            throw new IllegalArgumentException("The given rank must be greater than zero"); //$NON-NLS-1$
         } else if (size < 1) {
-            throw new IllegalArgumentException("The given size must be greater than one");
+            throw new IllegalArgumentException("The given size must be greater than one"); //$NON-NLS-1$
         } else if (rank >= size) {
-            throw new IllegalArgumentException("The given rank must be less than given size");
+            throw new IllegalArgumentException("The given rank must be less than given size"); //$NON-NLS-1$
         }
 
         if (rank == 0) {
@@ -408,7 +415,7 @@ public class Main {
 
         for (int i = 0; i < size; i++) {
             if (rank == i) {
-                System.out.println("Rank: " + rank);
+                System.out.println("Rank: " + rank); //$NON-NLS-1$
                 System.out.println(Arrays.toString(array.toArray()));
             }
 

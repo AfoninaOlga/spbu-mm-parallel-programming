f9d357fba3489185435d7fe75b22a25d87c4ed31
Max Nigmatulin <mvnigma@gmail.com>
2023-03-14 22:36:16 +0300

    Add refactor reminder in build.gradle.kts

--- MaximNigmatulin/Task1/build.gradle.kts
+++ MaximNigmatulin/Task1/build.gradle.kts
@@ -2,7 +2,6 @@ import java.io.File.separatorChar as sep
 
 plugins {
     kotlin("jvm") version "1.8.0"
-
     application
 }
 
@@ -12,14 +11,15 @@ repositories {
 
 val appMainClass = "hyper_quicksort.app.MainKt"
 
-//val mpjHome = System.getenv("MPJ_HOME") ?: error("Specify `MPJ_HOME` environment variable")
+// TODO: fix hardcoded value
+// val mpjHome = System.getenv("MPJ_HOME") ?: error("Specify `MPJ_HOME` environment variable")
 val mpjHome = "/Users/emax/Data/mpj"
 val mpjStarterJar = files("$mpjHome${sep}lib${sep}starter.jar")
 val mpjJar = files("$mpjHome${sep}lib${sep}mpj.jar")
 val mpjClassPath = sourceSets.main.get().runtimeClasspath - mpjJar
 
 
-// раскидать по аргументам
+// TODO: change positional args to string args
 val (numberOfProcesses, inputFilename, outputFilename) = (project.properties["args"] as? String? ?: "")
     .split(" ")
     .filterNot { it.isBlank() }
@@ -44,7 +44,11 @@ java {
 tasks.withType<JavaExec> {
     mainClass.set("runtime.starter.MPJRun")
     classpath = mpjStarterJar
-    args = listOf(appMainClass) + listOf("-cp", mpjClassPath.asPath) + listOf("-np", numberOfProcesses) +
-            inputFilename + outputFilename
+    args = listOf(appMainClass) +
+            listOf("-cp", mpjClassPath.asPath) +
+            listOf("-np", numberOfProcesses) +
+            inputFilename +
+            outputFilename
+
     dependsOn("classes")
 }

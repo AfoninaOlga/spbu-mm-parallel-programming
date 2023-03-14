01b1c51265872650569e150c02af599eefff5b32
Max Nigmatulin <mvnigma@gmail.com>
2023-03-14 22:37:16 +0300

    Add IO

--- MaximNigmatulin/Task1/src/main/kotlin/hyper_quicksort/app/Main.kt
+++ MaximNigmatulin/Task1/src/main/kotlin/hyper_quicksort/app/Main.kt
@@ -1,17 +1,23 @@
 package hyper_quicksort.app
 
+import hyper_quicksort.input_output.generateUnsorted
+import hyper_quicksort.input_output.read
+import hyper_quicksort.input_output.write
 import hyper_quicksort.seq_quicksort.quicksort
 import mpi.MPI
 
 fun main(args: Array<String>) {
     MPI.Init(args)
 
-    val testArray = arrayOf(5, 1, 4, 2, 3)
-    quicksort(testArray)
-    println(testArray.joinToString(" "))
+    val input = "./resources/unsorted.txt"
+    val output = "./resources/sorted.txt"
 
-    val rank = MPI.COMM_WORLD.Rank()
-    val size = MPI.COMM_WORLD.Size()
-    println("rank=$rank, size=$size")
+    if (MPI.COMM_WORLD.Rank() == 0) {
+        generateUnsorted(input, 10, 100)
+        val testArray = read(input)
+        quicksort(testArray)
+        println(testArray.joinToString(" "))
+        write(output, testArray.joinToString(" "))
+    }
     MPI.Finalize()
 }
---
+++ MaximNigmatulin/Task1/src/main/kotlin/hyper_quicksort/input_output/IO.kt
@@ -0,0 +1,27 @@
+package hyper_quicksort.input_output
+
+import java.io.File
+import java.io.FileWriter
+
+
+fun read(fileName: String): Array<Int> {
+    return File(fileName)
+        .readText()
+        .split(" ")
+        .map { it.toInt() }.toTypedArray()
+}
+
+fun write(fileName: String, data: String) {
+    File(fileName).writeText(data)
+}
+
+fun generateUnsorted(outFile: String, nNumbers: Int, upperLimit: Int) {
+    val fileWriter = FileWriter(outFile)
+    for (i in 0 until nNumbers) {
+        val num = (0..upperLimit).random()
+        fileWriter.write("$num ")
+    }
+    val num = (0..upperLimit).random()
+    fileWriter.write("$num")
+    fileWriter.close()
+}

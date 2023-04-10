f8648539c8bb89e564fe7b78594065c707575b97
= <hrasaa1@gmail.com>
2023-04-10 23:35:05 +0300

    Task1 added

---
+++ KarimiHurmatullah/Task1/PrimAlgorithm/PrimAlgorithm/Edges/Edge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimAlgorithm.Edges
+{
+    [Serializable]
+    public struct Edge
+    {
+        public int VertexFrom { get; }
+        public int VertexTo { get; }
+        public int Weight { get; }
+
+        public Edge(int vertexFrom, int vertexTo, int weight)
+        {
+            VertexFrom = vertexFrom;
+            VertexTo = vertexTo;
+            Weight = weight;
+        }
+    }
+}

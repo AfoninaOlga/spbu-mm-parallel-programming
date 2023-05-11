f482d3e1d827de7b7e7c87bb175a2acd963435bc
Max Nigmatulin <mvnigma@gmail.com>
2023-05-11 08:49:36 +0300

    Fix old code

--- MaximNigmatulin/Task4/test_infra/launch-jmeter.sh
+++ MaximNigmatulin/Task4/test_infra/launch-jmeter.sh
@@ -21,19 +21,19 @@ do
     container_hash=$(docker run -p 8080:8080 -d exam-system)
     sleep 10
     source pre-populate100.sh "http://0.0.0.0:8080/$system"
-    $jmeter -n -t "test_plans/Add load $(rps)rps ($system).jmx"
+    $jmeter -n -t "test_plans/Add load ${rps}rps ($system).jmx"
     docker stop "$container_hash" && docker rm "$container_hash"
 
     container_hash=$(docker run -p 8080:8080 -d exam-system)
     sleep 10
     source pre-populate100.sh "http://0.0.0.0:8080/$system"
-    $jmeter -n -t "test_plans/Contains load $(rps)rps ($system).jmx"
+    $jmeter -n -t "test_plans/Contains load ${rps}rps ($system).jmx"
     docker stop "$container_hash" && docker rm "$container_hash"
 
     container_hash=$(docker run -p 8080:8080 -d exam-system)
     sleep 10
     source pre-populate4900.sh "http://0.0.0.0:8080/$system"
-    $jmeter -n -t "test_plans/Remove load $(rps)rps ($system).jmx"
+    $jmeter -n -t "test_plans/Remove load ${rps}rps ($system).jmx"
     docker stop "$container_hash" && docker rm "$container_hash"
   done
 done
